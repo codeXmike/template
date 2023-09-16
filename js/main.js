069150dc72139644577f26493d85a12ed36a68c1
codeXmike <145227447+codeXmike@users.noreply.github.com>
2023-09-16 16:25:32 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: ["<img src='./assets/nav2.png'>", "<img src='./assets/nav.png'>"]
+    });
+});
