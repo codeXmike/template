00d6362c43da313436623fc6dee7c585bd8d6b18
codeXmike <145227447+codeXmike@users.noreply.github.com>
2023-09-17 03:46:32 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    
+    $nav = $('.nav')
+    $toggleCollapse = $('.toggle-collapse')
+
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');
+    })
 
     $('.owl-carousel').owlCarousel({
         loop: true,
